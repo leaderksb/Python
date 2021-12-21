37dd350632d16bf285116f601219a4eccc69d2a2
remnantleader <rtleader1228@gmail.com>
2021-12-21 22:56:45 +0900

    탈출문자

---
+++ 탈출문자.py
@@ -0,0 +1,21 @@
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
+
+# \" \' : 문장 내에서 따옴표
+print("저는 '김수빈'입니다.")
+print('저는 "김수빈"입니다.')
+print("저는 \"김수빈\"입니다.")
+print("저는 \'김수빈\'입니다.")
+
+# \\ : 문장 내에서 \
+print("C:\\Users\\remnantleader")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")
+print("Yellow Banana\rPuree")
+
+# \b : 백스페이스 (한글자 삭제)
+print("Bluee\bSky")
+
+# \t : 탭
+print("Green\tGrass")

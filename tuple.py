b668d789e03bcbf34180441e92abdcea2d04b545
remnantleader <rtleader1228@gmail.com>
2021-12-23 17:53:40 +0900

    튜플

--- tuple.py
+++ tuple.py
@@ -1,12 +1,13 @@
-menu = ("돈까스", "치즈까스")
+menu = ("돈가스", "치즈가스")
 print(menu[0])
 print(menu[1])
 
-# menu.add("생선까스")
+# menu.add("생선가스")
 # 튜플은 불변성을 가지고 있다
 
-name = "김종국"
-age = 20
-hobby = "코딩"
-(name, age, hobby) = ("김종국", 20, "코딩")
+# name = "김수빈"
+# age = 20
+# hobby = "코딩"
+
+(name, age, hobby) = ("김수빈", 20, "코딩")
 print(name, age, hobby)

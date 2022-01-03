5998407480e8f992a432d105cf866b11b1fd3928
remnantleader <rtleader1228@gmail.com>
2022-01-03 23:16:35 +0900

    지역변수와 전역변수

--- 지역변수와전역변수.py
+++ 지역변수와전역변수.py
@@ -14,3 +14,13 @@ print("전체 총 : {0}".format(gun))
 # checkpoint(2)  # 2명이 경계근무 나감
 gun = checkpoint_ret(gun, 2)  # 지역변수를 전역변수에 저장
 print("남은 총 : {0}".format(gun))
+
+# 표준 체중을 구하는 프로그램
+def std_weight(h, g):
+    m = h / 100  # 키는 m 단위
+    if g == "남자":
+        return print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(h, g, round(m * m * 22, 2)))
+    elif g == "여자":
+        return print("키 {0}cm {1}의 표준 체중은 {2:.2f}kg 입니다.".format(h, g, m * m * 21))
+
+std_weight(163, "여자")

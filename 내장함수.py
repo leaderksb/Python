c000e8b8ba33aad5963a71592f0eb63b12fc1374
remnantleader <rtleader1228@gmail.com>
2022-01-06 19:05:51 +0900

    내장 함수

---
+++ 내장함수.py
@@ -0,0 +1,18 @@
+# input : 사용자 입력을 받는 함수
+language = input("무슨 언어를 좋아하세요? ")
+print("{0}은 아주 좋은 언어입니다!".format(language))
+
+# dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+print(dir())
+import random  # 외장 함수
+print(dir())
+import pickle
+print(dir())
+
+lst = [1, 2, 3]
+print(dir(lst))
+
+name = "Kim"
+print(dir(name))
+
+# 구글에서 'list of python modules'로 검색 시 내장 함수에 대한 많은 정보 제공
